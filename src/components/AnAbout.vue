ca0daae7ddaaa69e44db0fbdce03fbffaab4fa00
Bob Walsh <bob.walsh@47hats.com>
2020-02-14 16:49:07 -0800

    Make About page data-driven

---
+++ src/components/AnAbout.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class=''>
+    <g-image
+        alt="Cover image"
+        v-if="about.data.author_image"
+        class="about__image g-image"
+        :src="about.data.author_image"
+      />
+    <h1 class="study__title" v-html="about.data.first_last" />
+    <h3 class="study__title" v-html="about.data.tagline" />
+    <p class="study__description" v-html="about.data.body_text1" />
+    <p class="study__description" v-html="about.data.body_text2" />
+    <p class="study__description" v-html="about.data.body_text3" />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: ["about"]
+};
+</script>
+
+<style lang="scss">
+.about {
+  margin-bottom: var(--space);
+  position: relative;
+
+  &__content-box {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+  }
+
+  &__header {
+    margin-left: calc(var(--space) * -1);
+    margin-right: calc(var(--space) * -1);
+    margin-bottom: calc(var(--space) / 2);
+    margin-top: calc(var(--space) * -1);
+    overflow: hidden;
+    border-radius: var(--radius) var(--radius) 0 0;
+    flex-basis: 25%;
+    &:empty {
+      display: none;
+    }
+  }
+  &__content {
+    flex-basis: 75%;
+  }
+  &__image {
+    border: 2px solid gray;
+    width: 20%;
+    margin-left: 20px;
+    margin-right: auto;
+    box-shadow: 1px 10px 30px 0 rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 5px;
+    align-content: center;
+  }
+  &__title {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  &__description {
+    margin-top: 0;
+    display: block;
+    margin-bottom: 20px;
+    margin-block-start: 0em;
+    margin-block-end: 1rem;
+    margin-inline-start: 0px;
+    margin-inline-end: 1rem;
+  }
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 1px 10px 30px 0 rgba(0, 0, 0, 0.1);
+  }
+
+  &__tags {
+    z-index: 1;
+    position: relative;
+  }
+
+  &__link {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    overflow: hidden;
+    text-indent: -9999px;
+    z-index: 0;
+  }
+}
+</style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,48 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>About the Author</h1>
+      <AnAbout 
+          v-for="edge in $page.abouts.edges"
+          :key="edge.node.id"
+          :about="edge.node"
+          />
+
+
+
+
+      
   </Layout>
 </template>
 
-<script>
-export default {
-  metaInfo: {
-    title: 'About us'
+
+<page-query>
+query {
+  abouts: allButterPages{
+    edges {
+      node {
+        data {
+          first_last
+          tagline
+          body_text1
+          body_text2
+          body_text3
+          author_image
+        }
+      }
+    }
   }
 }
+</page-query>
+
+<script>
+import AnAbout from "~/components/AnAbout.vue";
+
+export default {
+  components: {
+    AnAbout
+  },
+  metaInfo: {
+    title: "Hello, ButterCMS!"
+  }
+};
 </script>
